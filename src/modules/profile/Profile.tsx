e9ae45b00a3d67d3aef78ef3c16909b579865711
truquest <{support@truquest.net}+{Truinfo}@users.noreply.github.com>
2024-09-06 17:21:49 +0530

    profile

--- src/modules/profile/Profile.tsx
+++ src/modules/profile/Profile.tsx
@@ -1,5 +1,5 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-
+import { IonAvatar, IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import './Profile.css';
 const Profile: React.FC = () => {
     return (
         <IonPage>
@@ -11,7 +11,18 @@ const Profile: React.FC = () => {
                     <IonTitle>Profile</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent>
+            <IonContent fullscreen >
+                <div className="profile-container">
+                    <IonAvatar className="profile-avatar">
+                        <img src="https://www.gravatar.com/avatar?d=mp" alt="Avatar" />
+                    </IonAvatar>
+                    <div className="profile-details">
+                        <h2>John Doe</h2>
+                        <p>+123 456 7890</p>
+                        <p>john@gmail.com</p>
+                        <p>Block-B,Flat No-101</p>
+                    </div>
+                </div>
 
             </IonContent>
         </IonPage>

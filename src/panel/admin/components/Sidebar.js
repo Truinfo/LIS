7504f63841cff0eeb026bfa679ce123dd52b7bab
truquest <{support@truquest.net}+{Truinfo}@users.noreply.github.com>
2024-09-23 11:35:20 +0530

    floder added

--- src/panel/admin/components/Sidebar.js
+++ src/panel/admin/components/Sidebar.js
@@ -2,18 +2,21 @@ import * as React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Icon from 'react-native-vector-icons/FontAwesome'; // Import FontAwesome icons
-import Dashboard from '../pages/Dashboard';
-import Advertisements from '../pages/Advertisements';
-import ResidentialUnit from '../pages/ResidentialUnit';
-import UserManagement from '../pages/UserManagement';
-import Events from '../pages/Events';
-import Polls from '../pages/Polls';
-import VisitorManagement from '../pages/VisistorManagement';
-import Amenities from '../pages/Amenities';
-import Inventory from '../pages/Inventory';
-import Complaints from '../pages/Complaints';
-import Documents from '../pages/Documents';
-import SocietyBills from '../pages/SocietyBills';
+import Dashboard from '../pages/Dashboard/Dashboard';
+import Advertisements from '../pages/Advertisements/Advertisements';
+import ResidentialUnit from '../pages/ResidentialUnit/ResidentialUnit';
+import UserManagement from '../pages/UserManagement/UserManagement';
+import Events from '../pages/Events/Events';
+import Polls from '../pages/Polls/Polls';
+import VisitorManagement from '../pages/VisistorManagement/VisistorManagement';
+import Amenities from '../pages/Amenities/Amenities';
+import Inventory from '../pages/Inventory/Inventory';
+import Complaints from '../pages/Complaints/Complaints';
+import Documents from '../pages/Documents/Documents';
+import SocietyBills from '../pages/SocietyBills/SocietyBills';
+import Security from '../pages/Security/Security';
+import Services from '../pages/Services/Services';
+import Maintenance from '../pages/Maintenance/Maintenance';
 
 const Drawer = createDrawerNavigator();
 
@@ -26,13 +29,12 @@ export default function Sidebar() {
           <View style={{ flexDirection: 'row', alignItems: "center", gap: 15, marginRight: 10 }}>
 
             <View>
-              <Icon name="bell-o" size={25} color="#000" />
+              <Icon name="bell-o" size={25} color="#202020" />
               <View style={styles.badge}>
                 <Text style={styles.badgeText}>3</Text>
               </View>
             </View>
-            <Icon name="user-circle-o" size={25} color="#000" />
-
+            <Icon name="user-circle-o" size={25} color="#202020" />
           </View>
         ),
       }}
@@ -40,15 +42,18 @@ export default function Sidebar() {
       <Drawer.Screen name="Dashboard" component={Dashboard} />
       <Drawer.Screen name="Advertisements" component={Advertisements} />
       <Drawer.Screen name="Residential Unit" component={ResidentialUnit} />
-      <Drawer.Screen name="UserManagement" component={UserManagement} />
+      <Drawer.Screen name="User Management" component={UserManagement} />
+      <Drawer.Screen name="Security" component={Security} />
+      <Drawer.Screen name="Services" component={Services} />
       <Drawer.Screen name="Complaints" component={Complaints} />
       <Drawer.Screen name="Documents" component={Documents} />
       <Drawer.Screen name="Events" component={Events} />
       <Drawer.Screen name="Polls" component={Polls} />
-      <Drawer.Screen name="VisitorManagement" component={VisitorManagement} />
+      <Drawer.Screen name="Visitor Management" component={VisitorManagement} />
       <Drawer.Screen name="Amenities" component={Amenities} />
       <Drawer.Screen name="Inventory" component={Inventory} />
       <Drawer.Screen name="Society Bills" component={SocietyBills} />
+      <Drawer.Screen name="Maintenance Bills" component={Maintenance} />
     </Drawer.Navigator>
   );
 }
